cb11ce5492e5c0e2d8b9e38de3922188a5a5654e
Terenz <terence592592@gmail.com>
2025-01-25 17:36:56 +0800

    deploy setting

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ Created on Mon Jan 20 18:00:28 2025
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from predict import get_cat_breed
+import os
 
 app = Flask(__name__)
 CORS(app)  # 启用 CORS，允许跨域请求
@@ -46,5 +47,10 @@ def predictBreed():
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    # 如果在开发环境中，使用 Flask 自带的服务器
+    if os.getenv('FLASK_ENV') == 'development':
+        app.run(host='localhost', port=5000)
+    else:
+        # 在生产环境中，通过 gunicorn 启动 Flask
+        app.run(host='0.0.0.0', port=5000)  # 这里可以选择其他端口
 
